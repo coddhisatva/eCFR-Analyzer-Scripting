4aac4e7ed7333899514f1aba30a133d99fbc2f41
skaymore <conoregan151@gmail.com>
2025-04-02 10:40:20 -0700

    HERE IS NODE COMP FUNCTION

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ecfr-analyzer",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "supabase",
+    ],
+) 
